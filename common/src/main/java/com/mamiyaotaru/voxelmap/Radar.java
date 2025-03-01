6541263a7220e24802a9b38bff59b11b23941058
감자 <142703479+Algamja11@users.noreply.github.com>
2025-03-01 17:02:11 +0900

    leather armor color fix 2

--- common/src/main/java/com/mamiyaotaru/voxelmap/Radar.java
+++ common/src/main/java/com/mamiyaotaru/voxelmap/Radar.java
@@ -1295,7 +1295,7 @@ public class Radar implements IRadar {
             } else if (helmet instanceof ArmorItem helmetArmor) {
                 if (this.isLeatherArmor(helmetArmor)) {
                     icon = this.textureAtlas.getAtlasSpriteIncludingYetToBeStitched("armor " + this.armorNames[0]);
-                    contact.setArmorColor(DyedItemColor.getOrDefault(stack, 0xA06540));
+                    contact.setArmorColor(DyedItemColor.getOrDefault(stack, DyedItemColor.LEATHER_COLOR));
                 } else {
                     boolean isPiglin = contact.type == EnumMobs.PIGLIN || contact.type == EnumMobs.ZOMBIFIED_PIGLIN;
                     icon = this.textureAtlas.getAtlasSpriteIncludingYetToBeStitched("armor " + helmet.getDescriptionId() + (isPiglin ? "_piglin" : ""));
